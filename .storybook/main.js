5836acb8245699437642e954f7584d98634b4e67
Sixtus Anyanwu <sikky606@gmail.com>
2022-05-15 18:17:21 +0100

    updated README

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@chakra-ui/storybook-addon'],
-  features:{
-    emotionAlias: false
-  }
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@chakra-ui/storybook-addon',
+  ],
+  features: {
+    emotionAlias: false,
+  },
   typescript: {
     check: true, // type-check stories during Storybook build
   },
@@ -18,6 +23,6 @@ module.exports = {
           'emotion-theming': '@emotion/react',
         },
       },
-    }
+    };
   },
-}
+};
