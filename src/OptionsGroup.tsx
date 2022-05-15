2bf46fdf570ade08f325ce806e637749dbdb877a
EmmanuelObi <63916312+EmmanuelObi@users.noreply.github.com>
2022-05-16 00:03:55 +0100

    touched selectDefault file. added placeholderColor and focusedbordercolor prop

--- src/OptionsGroup.tsx
+++ src/OptionsGroup.tsx
@@ -1,23 +1,45 @@
 import React, { ReactNode } from 'react';
-import {
-  listStyle,
-  defaultStyles,
-} from './styles/default';
+import { listStyle, defaultStyles } from './styles/default';
+import { styled } from '@stitches/react';
 
 export interface OptionsGroupProps {
-	children: ReactNode | ReactNode[];
-    variant?:"lined" | "unlined";
+  children: ReactNode | ReactNode[];
+  variant?: 'lined' | 'unlined';
 }
 
-export const OptionsGroup = ({children, variant = "unlined", ...props}: OptionsGroupProps) => {
+export const OptionsGroup = ({
+  children,
+  variant,
+  ...props
+}: OptionsGroupProps) => {
+  console.log({ variant });
+  const BaseOption = styled('div', {
+    variants: {
+      variant: {
+        unlined: {
+          '& li': {},
+        },
+        lined: {
+          '& li': {
+            borderBottom: '1px solid #BDBDBD',
+          },
+        },
+      },
+    },
+    defaultVariants: {
+      variant: 'unlined',
+    },
+  });
 
-    return (
-        <div style={{ ...defaultStyles }} {...props} role="listbox" tabIndex={-1}>
-                  <ul style={{ ...listStyle }}>
-						{children}
-                  </ul>  
-                </div>
-    );
+  return (
+    <BaseOption
+      variant={variant}
+      style={{ ...defaultStyles }}
+      {...props}
+      role="listbox"
+      tabIndex={-1}
+    >
+      <ul style={{ ...listStyle }}>{children}</ul>
+    </BaseOption>
+  );
 };
-
-
