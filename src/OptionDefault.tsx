da570bf3c957fbdeeb4560183b9ca793f89af64b
Maureen Anyanwu <maureenanyanwu519@gmail.com>
2022-05-17 19:02:31 +0100

    added options props

--- src/OptionDefault.tsx
+++ src/OptionDefault.tsx
@@ -2,9 +2,8 @@ import React, {ReactNode} from 'react';
 import {  CheckIcon } from '@chakra-ui/icons';
 import {
   listItemButtonStyle,
-  listItemStyle,
-  //listItemStyleDisabled,
 } from './styles/default';
+import { styled } from '@stitches/react';
 
 export interface OptionProps {
   children: string;
@@ -13,34 +12,80 @@ export interface OptionProps {
   leftIcon?: ReactNode;
   rightIcon?: ReactNode;
   value: string;
-  //disabled?: boolean;
+  disabled?: boolean;
 }
 
-export const OptionDefault = ({ handleOnClick, isItemInSelection, children, leftIcon, rightIcon, value }: OptionProps ) => {
+export const OptionDefault = ({ handleOnClick, isItemInSelection, disabled=false, children, leftIcon, rightIcon, value }: OptionProps ) => {
+
+  //base styles for option
+  const BaseOption = styled('li', {
+    listStyleType: 'none',
+    borderTopLeftRadius: '4px',
+    borderTopRightRadius: '4px',
+    backgroundColor: 'white',
+    fontWeight: 600,
+    fontSize: '13px',
+    padding: '8px 20px',
+    width: '100%',
+    margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'space-between',
+    '&:hover': {
+      backgroundColor: '#EDF2F7',
+    },
+  })
+
+  function DisabledAndSelectedStyles(){
+    if(disabled){
+      return {
+        backgroundColor:'#EDF2F7',
+        color:'#CBD5E0',
+      }
+    }else{
+      if(isSelected(value)){
+        return {
+          backgroundColor:'#3182CE',
+          color:'#fff',
+        }
+      } else{
+        return {
+          outline:'none',
+        }
+      }
+    }
+  }
 
 	const isSelected = (x:string) => {
 			return isItemInSelection(x)
 	}
 
 	const handleClick = () => {
-		handleOnClick(children);
+    if(!disabled){
+      handleOnClick(children);
+    } else{
+      return
+    }
+		
 	}
 
 	//const style = {disabled ? listItemStyleDisabled : listItemStyle}
 
     return (
-        <li style={{...listItemStyle}} role="option" id={value} tabIndex={0}>
+        <BaseOption role="option" key={value} tabIndex={0} style={DisabledAndSelectedStyles()}>
                         <button
                           type="button"
                           style={{ ...listItemButtonStyle }}
                           onClick={handleClick}
+                          id={value}
                         >
-                          {leftIcon ? <span>{leftIcon} &nbsp; &nbsp;</span> : null}
-                          <span>{children}</span>
-                          {rightIcon ? <span> &nbsp; &nbsp;{rightIcon}</span> : null}
-                          <span>{isSelected(children) && <CheckIcon />}</span>
+                          <span>
+                            {leftIcon ? <span>{leftIcon} &nbsp; &nbsp;</span> : null}
+                            <span>{children}</span>
+                            {rightIcon ? <span> &nbsp; &nbsp;{rightIcon}</span> : null}
+                          </span>
                         </button>
-                 </li> 
+                        {isSelected(value) ? (<span><CheckIcon /></span>) : null }
+                 </BaseOption> 
     );
 };
 
---
+++ stories/OptionDefault.stories.tsx
@@ -0,0 +1,33 @@
+// import React from 'react';
+// import { Meta, Story } from '@storybook/react';
+// import { OptionDefault, OptionProps } from '../src/OptionDefault';
+// //import {InfoIcon} from '@chakra-ui/icons';
+// import {SelectDefault} from '../src/SelectDefault'
+
+// const meta: Meta = {
+//   title: 'Select Option Component',
+//   component: OptionDefault,
+//   argTypes: {
+//     value: {
+//       defaultValue: "1",
+//     },
+//     children: {
+//       defaultValue: "Option 1",
+//     },
+//   },
+//   parameters: {
+//     controls: { expanded: true },
+//   },
+// };
+
+// export default meta;
+
+// const Template: Story<OptionProps> = (args) => {
+// <SelectDefault placeholder="Select one option" defaultValue="">
+//   <OptionDefault {...args}/>
+//   </SelectDefault>
+// }
+  
+
+// export const Default= Template.bind({});
+
