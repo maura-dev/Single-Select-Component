224607920be321ef326295e3a17935e6873ed75a
Sixtus Anyanwu <sikky606@gmail.com>
2022-05-18 14:03:03 +0100

    installed some stuff

--- example/index.tsx
+++ example/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div></div>;
+  return <div> Hello </div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
